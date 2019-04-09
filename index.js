9f24d37587512d61f4b64028c3deeebd00caf972
CarsonBain <carsonbain@gmail.com>
2019-04-09 00:15:05 -0700

    add dark mode and other small changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import "./styles/index.scss";
-import { settings } from "./js/settings";
+import './styles/index.scss';
+import { settings } from './js/settings';
 
-const proxy = "https://cors-anywhere.herokuapp.com/";
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 const DSAPIkey = settings.DSAPIkey;
 const GAPIkey = settings.GAPIkey;
 
@@ -11,7 +11,7 @@ const getPosition = () => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
   } else {
-    alert("Uh oh, geolocation is not supported by this browser");
+    alert('Uh oh, geolocation is not supported by this browser');
   }
 };
 
@@ -23,32 +23,46 @@ getPosition()
     const getDarkSky = fetch(DSAPIurl)
       .then(response => response.json())
       .then(data => {
-        document.querySelector(".currently span").innerHTML =
+        console.log(data);
+        document.querySelector('.currently span').innerHTML =
           data.minutely.summary;
-        document.querySelector(".precip-prob span").innerHTML = `${
+        document.querySelector('.precip-prob span').innerHTML = `${
           data.currently.precipProbability
         }%`;
         document.querySelector(
-          ".temperature"
+          '.temperature'
         ).innerHTML = `${data.currently.temperature.toFixed(
           0
         )}<span class='wi wi-degrees'></span>`;
-        let icon = "wi wi-forecast-io-" + data.currently.icon;
+        let icon = 'wi wi-forecast-io-' + data.currently.icon;
         document.querySelector(
-          ".icon"
+          '.icon'
         ).innerHTML = `<span class='${icon}'></span>`;
       });
 
     const getGoogle = fetch(GAPIurl)
       .then(response => response.json())
       .then(data => {
-        document.querySelector(".city-name").innerHTML =
+        document.querySelector('.city-name').innerHTML =
           data.results[7].formatted_address;
       });
     Promise.all([getDarkSky, getGoogle]).then(() => {
-      document.querySelector(".loading").style.display = "none";
+      document.querySelector('.loading').style.display = 'none';
     });
   })
   .catch(err => {
     console.log(err.message);
   });
+
+// 🌃 Mode
+const toggleInput = document.querySelector('#dark-toggle');
+
+toggleInput.addEventListener('click', toggleDarkMode);
+
+function toggleDarkMode() {
+  if (toggleInput.checked) {
+    //lights out
+    document.querySelector('body').classList.add('dark');
+  } else document.querySelector('body').classList.remove('dark');
+}
+//TODO: Save state of darkmode in localstorage
